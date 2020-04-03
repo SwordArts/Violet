3a98239ed79bc43154d08a403b4254ff893b1d7f
SamuraiStacks <38820196+SamuraiStacks@users.noreply.github.com>
2020-04-03 16:09:23 -0400

    clean up and final touched. added usage

--- commands/System/help.js
+++ commands/System/help.js
@@ -6,6 +6,7 @@ class HelpCommand extends BaseCommand {
 			info: {
 				name: "help",
 				description: "Shows all the commands that I have."
+				usage: "help [command]"
 			},
 			settings: {
 				aliases: ["h", "commands"],
@@ -28,14 +29,26 @@ class HelpCommand extends BaseCommand {
                 }
             }
             msg.channel.send({embed: {
-                color: 0x0,
-                fields: fields
+                color: 0x47aef0,
+                thumbnail: {
+                        url: violet.user.displayAvatarURL({ format: "png", dynamic: true, size: 512})
+                    },
+                author: {
+                    name: `Violet's Commands`,
+                    icon_url: msg.author.displayAvatarURL()
+                },
+                description: "Each field represents a category. If you want to find more detail about a command, use `violet help [command]` for example `violet help ping`",
+                fields: fields,
+                footer: {
+                    	text: `Requested by ${msg.author.tag}`
+                    },
+                timestamp: Date.now()
             }})
         } else {
             if(violet.commands.has(args[0])) {
                 const cmd = violet.commands.get(args[0])
                 msg.channel.send({embed: {
-                    color: 0x0,
+                    color: 0x47aef0,
                     thumbnail: {
                         url: violet.user.displayAvatarURL({ format: "png", dynamic: true, size: 512})
                     },
@@ -43,7 +56,11 @@ class HelpCommand extends BaseCommand {
                         name: `Details for the command ${args[0]}.`,
                         icon_url: msg.author.displayAvatarURL()
                     },
-                    description: `${cmd.info.description}\nAliases: ${cmd.settings.aliases.length !== 0 ? `\`${cmd.settings.aliases.join("`, `")}\`` : "None."}`
+                    description: `\`${cmd.info.usage}\`\n${cmd.info.description}\nAliases: ${cmd.settings.aliases.length !== 0 ? `\`${cmd.settings.aliases.join("`, `")}\`` : "None."}`,
+                    footer: {
+                    	text: `[] is optional, <> is needed. Don't include <> or []. Requested by ${msg.author.tag}`
+                    },
+                    timestamp: Date.now()
                 }})
             } else {
                 msg.channel.send("Sorry, I could not find that command.")
